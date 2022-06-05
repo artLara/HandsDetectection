4ebeda8924b7a1628a047b514a633cbaa5cdb0c0
artLara <art.lara.caz@gmail.com>
2022-06-05 01:38:27 -0500

    test2

---
+++ preprocesado.py
@@ -0,0 +1,84 @@
+from os import walk
+from shutil import copyfile
+from cv2 import cv2
+from pandas import pandas as pd
+# tipo = 'maligno'
+
+def preprocess():
+    path = 'D:\\CIC\\Matematicas\\Object'
+
+    # f = []
+    # dirpath, dirnames, filenames = walk(path)
+
+    print(path)
+    dst = 'D:\\CIC\\Matematicas\\HandsDetectection\\Object\\'
+    for (dirpath, dirnames, filenames) in walk(path):
+        # f.extend(filenames)
+        # print(filenames)
+        imageCount = 0
+
+        for (index, name) in enumerate(filenames):
+            # print(name, index) 
+            # elements = name.split('-')
+            # zoom = elements[3]
+            # num = elements[4]
+            # print(dirpath+'\\'+name)
+            image = cv2.imread(dirpath+'\\'+name, cv2.IMREAD_UNCHANGED)
+            image = cv2.resize(image, (300,300), interpolation = cv2.INTER_AREA)
+            # print(name)
+            name_without_extension = name.split(".")[0]
+            # print(name_without_extension)
+            cv2.imwrite(dst+name_without_extension+'.jpg', image)
+
+
+            # print('zoom = {} num={}'.format(zoom, num))
+            # print(dst+zoom+'x\\maligno\\'+name)
+            # copyfile(dirpath+'\\'+name, dst+'\\'+name)
+
+            return
+
+def preTrain():
+    path = 'D:\\CIC\\Matematicas\\ReconocedorAlfabeto\\asl_alphabet_train\\asl_alphabet_train'
+    df = pd.DataFrame()
+    imageNames = []
+    classes = []
+    # f = []
+    # dirpath, dirnames, filenames = walk(path)
+
+    print(path)
+    dst = 'D:\\CIC\\Matematicas\\ReconocedorAlfabeto\\vocales'
+    for (dirpath, dirnames, filenames) in walk(path):
+        # f.extend(filenames)
+        # for signal in dirnames:
+        
+        # print(filenames)
+        for (index, name) in enumerate(filenames):
+            # print(name) 
+            # elements = name.split('-')
+            # zoom = elements[3]
+            # num = elements[4]
+            # print(dirpath+'\\'+name)
+            # print('zoom = {} num={}'.format(zoom, num))
+            # print(dst+zoom+'x\\maligno\\'+name)
+            # copyfile(dirpath+'\\'+name, dst+'\\'+name)
+            # imageNames.append(name)
+            c = list(name)[0]
+            if c=='A' or c=='E' or c=='I' or c=='O' or c=='U':
+                # print(name, c)
+                imageNames.append(name)
+                classes.append(c)
+                # copyfile(dirpath+'\\'+name, dst+'\\'+name)
+
+
+
+            # gray = cv2.imread(dirpath+'\\'+name, 0)
+            # cv2.imwrite(dst+name, gray)
+            if index == 2000:
+                break
+
+    df['Name'] = imageNames
+    df['Class'] = classes
+    df.to_csv('D:\\CIC\\Matematicas\\ReconocedorAlfabeto\\vocales\\vocales2000.csv', index=False)
+# preTest()
+preprocess()
+print('Finish!!')
